08d4243a96c50de3af0d9e33be83ba5d3cd3d9d3
Ender <xenderpugx@gmail.com>
2019-04-09 10:08:44 +0100

    ❌ Legacy has been discontinued

--- wexond-legacy/src/renderer/app/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './design';
-export * from './tabs';
-export * from './icons';
--- wexond-legacy/src/renderer/app/models/history-item.ts
+++
@@ -1,9 +0,0 @@
-export interface HistoryItem {
-  _id?: string;
-  title?: string;
-  url?: string;
-  date?: string;
-  favicon?: string;
-  hovered?: boolean;
-  selected?: boolean;
-}
