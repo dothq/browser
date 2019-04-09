d14217b6ee751666b5d2d05390082128fb8b89e0
Ender <xenderpugx@gmail.com>
2019-04-09 18:27:33 +0100

    👨🏻‍💻 Updated Wexond for Dot

--- wexond/src/renderer/app/models/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './tab';
-export * from './tab-group';
-export * from './favicon';
-export * from './history-item';
-export * from './suggestion';
-export * from './browser-action';
--- wexond/src/renderer/constants/index.ts
+++
@@ -1,3 +0,0 @@
-export * from './fonts';
-export * from './transparency';
-export * from './colors';
