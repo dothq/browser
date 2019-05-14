fa12cb7f1e728d1e62c118ce21adfa634819c35a
Ender <xenderpugx@gmail.com>
2019-05-14 19:51:27 +0100

    👀 Added Tray Icon click function
    The tray icon will now focus the main window if clicked.

--- src/renderer/app/index.tsx
+++ src/renderer/app/index.tsx
@@ -11,6 +11,39 @@ import { AppWindow } from './app-window';
 import { resolve } from 'path';
 import console = require('console');
 
+const { remote } = require('electron')
+const { Menu, MenuItem, Tray, app } = remote
+
+var tray = new Tray(resolve(app.getAppPath(), 'static/app-icons/icon.png'))
+const contextMenu = Menu.buildFromTemplate([
+  { label: `Dot ${app.getVersion()}`, type: 'normal', enabled: false, icon: resolve(app.getAppPath(), 'static/app-icons/tray-icon.png') },
+  { type: 'separator' },
+  { label: 'History', type: 'normal', click() {
+      ipcRenderer.send('window-focus');
+      store.overlay.visible = true;
+      store.overlay.currentContent = "history";
+      store.overlay.scrollRef.current.scrollTop = 0;   
+  } },
+  { label: 'Bookmarks', type: 'normal', click() {
+    ipcRenderer.send('window-focus');
+    store.overlay.visible = true;
+    store.overlay.currentContent = "bookmarks";
+    store.overlay.scrollRef.current.scrollTop = 0;   
+  } },
+  { label: 'Settings', type: 'normal', click() {
+    ipcRenderer.send('window-focus');
+    store.overlay.visible = true;
+    store.overlay.currentContent = "settings";
+    store.overlay.scrollRef.current.scrollTop = 0;   
+  } },
+  { type: 'separator' },
+  { label: `Quit Dot ${app.getVersion()}`, type: 'normal', role: 'quit', icon: resolve(app.getAppPath(), 'static/app-icons/tray-close.png') },
+])
+tray.setToolTip(`Dot ${app.getVersion()}`)
+tray.on('click', () => {
+  ipcRenderer.send('window-focus');
+}) 
+
 ipcRenderer.setMaxListeners(0);
 
 const styleElement = document.createElement('style');
@@ -68,36 +101,9 @@ store.tabGroups.addGroup();
 
 export let appWindow: AppWindow;
 
-const { remote } = require('electron')
-const { Menu, MenuItem, Tray, app } = remote
-
-var tray = new Tray(resolve(app.getAppPath(), 'static/app-icons/icon.png'))
-const contextMenu = Menu.buildFromTemplate([
-  { label: `Dot ${app.getVersion()}`, type: 'normal', enabled: false, icon: resolve(app.getAppPath(), 'static/app-icons/tray-icon.png') },
-  { type: 'separator' },
-  { label: 'History', type: 'normal', click() {
-      ipcRenderer.send('window-focus');
-      store.overlay.visible = true;
-      store.overlay.currentContent = "history";
-      store.overlay.scrollRef.current.scrollTop = 0;   
-  } },
-  { label: 'Bookmarks', type: 'normal', click() {
-    ipcRenderer.send('window-focus');
-    store.overlay.visible = true;
-    store.overlay.currentContent = "bookmarks";
-    store.overlay.scrollRef.current.scrollTop = 0;   
-  } },
-  { label: 'Settings', type: 'normal', click() {
-    ipcRenderer.send('window-focus');
-    store.overlay.visible = true;
-    store.overlay.currentContent = "settings";
-    store.overlay.scrollRef.current.scrollTop = 0;   
-  } },
-  { type: 'separator' },
-  { label: `Quit Dot ${app.getVersion()}`, type: 'normal', role: 'quit', icon: resolve(app.getAppPath(), 'static/app-icons/tray-close.png') },
-])
-tray.setToolTip(`Dot ${app.getVersion()}`)
-tray.setContextMenu(contextMenu)
+setInterval(function() {
+  tray.setContextMenu(contextMenu)
+}, 250);
 
 Menu.setApplicationMenu(
   Menu.buildFromTemplate([
