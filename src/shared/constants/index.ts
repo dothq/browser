21141ff245afa79768befb1bec64d4c656f7c515
Jake Ward <thegaminggeek362@gmail.com>
2019-06-09 23:30:50 +0100

    Add OS info to user-agent

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -1,7 +1,8 @@
 import { app } from "electron";
+import * as os from 'os';
 
 export const USER_AGENT =
-  `Dot/${app.getVersion()}`;
+  `Dot/${app.getVersion()} (${os.platform()} ${os.release()})`;
 
 export const FALLBACK_USER_AGENT =
   `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.0 Safari/537.36`;
