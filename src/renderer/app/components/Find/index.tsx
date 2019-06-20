9502021ff72b4fe7aca2d79bea836d27b7d8c107
Ender <xenderpugx@gmail.com>
2019-06-20 20:47:33 +0100

    🎚 Added range langs

--- src/renderer/app/components/Find/index.tsx
+++ src/renderer/app/components/Find/index.tsx
@@ -153,7 +153,7 @@ export const Find = observer(() => {
         onKeyPress={onKeyPress}
         onChange={onInput}
         ref={store.findInputRef}
-        placeholder="Find in page"
+        placeholder={store.locale.uk.window[0].find_in_page}
         id="find"
       />
       <Occurrences>{selectedTab && selectedTab.findOccurrences}</Occurrences>
