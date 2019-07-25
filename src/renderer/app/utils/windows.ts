f7c130a7393980813e6db0630bebf8df75168470
Ender <xenderpugx@gmail.com>
2019-07-25 16:06:01 +0100

    ❌ Add tab close confirmation dialog

--- src/renderer/app/utils/windows.ts
+++ src/renderer/app/utils/windows.ts
@@ -1,9 +1,47 @@
 import { remote } from 'electron';
+import store from '../store';
+import { resolve } from 'path';
+var modal = require('electron-modal');
+
+async function areYouSure(tabSize: number) {
+  var ars = await modal.open(resolve(remote.app.getAppPath() + '\\static\\pages\\util\\quit-dot.html'), {
+    width: 504,
+    height: 179,
+    resizable: true,
+    center: true,
+    title: 'Quit Dot',
+    maximizable: false,
+    modal: true,
+    minimizable: false,
+    movable: false,
+    icon: resolve(remote.app.getAppPath() + '/static/app-icons/tray-close.png'),
+    titleBarStyle: 'hiddenInset',
+    autoHideMenuBar: true,
+    webPreferences: {
+      nodeIntegration: true
+    },
+    frame: true
+  }, { tabs: tabSize } )
+
+  ars.show()
+
+  remote.webContents.getFocusedWebContents().openDevTools()
+
+  ars.on('window-close', () => {
+    ars.hide()
+    getCurrentWindow().close();
+  })
+}
 
 export const getCurrentWindow = () => remote.getCurrentWindow();
 
 export const closeWindow = () => {
-  getCurrentWindow().close();
+  if(store.tabs.list.length == 0) {
+    getCurrentWindow().close();
+  }
+  else {
+    areYouSure(store.tabs.list.length)
+  }
 };
 
 export const minimizeWindow = () => {
