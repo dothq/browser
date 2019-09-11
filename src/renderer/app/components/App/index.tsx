88a642c9cb0e930425b4dcdb4681ac327a59fa20
EnderDev <endercraftergaming@gmail.com>
2019-09-11 20:15:00 +0100

    fixing

--- src/renderer/app/components/App/index.tsx
+++ src/renderer/app/components/App/index.tsx
@@ -5,7 +5,7 @@ import { createGlobalStyle } from 'styled-components';
 import { Style } from '~/renderer/app/style';
 import { Toolbar } from '../Toolbar';
 import { ipcRenderer, remote } from 'electron';
-import { Line, StyledApp, Screenshot} from './style';
+import { Line, StyledApp, Screenshot } from './style';
 import { WindowsButtons } from '../WindowsButtons';
 import store from '../../store';
 import { platform } from 'os';
@@ -24,29 +24,31 @@ const GlobalStyle = createGlobalStyle`${Style}`;
 export function checkLightMode() {
   if (platform() === 'win32') {
     var exec = require('child_process').exec;
-    exec('reg query HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -v SystemUsesLightTheme',
-      function (err: any, stdout: any, stderr: any) {
+    exec(
+      'reg query HKCU\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize -v SystemUsesLightTheme',
+      function(err: any, stdout: any, stderr: any) {
         if (err) {
-            console.debug("\n"+stderr);
+          console.debug('\n' + stderr);
         } else {
-            if(stdout.split("0x")[1] == 1) {
-              ipcRenderer.send('is-light-mode');
-            }
-            if(stdout.split("0x")[1] == 0) {
-              ipcRenderer.send('is-dark-mode');
-            }
+          if (stdout.split('0x')[1] == 1) {
+            ipcRenderer.send('is-light-mode');
+          }
+          if (stdout.split('0x')[1] == 0) {
+            ipcRenderer.send('is-dark-mode');
+          }
         }
-    });
+      },
+    );
   }
 }
-checkLightMode()
+checkLightMode();
 
-store.locale.load()
+store.locale.load();
 store.tabGroups.addGroup();
 
 window.onbeforeunload = () => {
   ipcRenderer.send('browserview-clear');
-  checkLightMode()
+  checkLightMode();
 };
 
 const errorLogPath = getPath('dot-errors.log');
@@ -54,20 +56,22 @@ const errorLogPath = getPath('dot-errors.log');
 var time = new Date().toUTCString();
 
 if (existsSync(errorLogPath)) {
-  appendFile(errorLogPath, `// Error log effective of 2.2.0, ${time}. Running ${platform()}, started renderer app.\n`, function(err) {
-
-  });
+  appendFile(
+    errorLogPath,
+    `// Error log effective of 2.2.0, ${time}. Running ${platform()}, started renderer app.\n`,
+    function(err) {},
+  );
 }
 
 export const App = observer(() => {
   return (
-      <StyledApp>
-          <GlobalStyle />
-          <Toolbar />
-          <Line />
-          <Screenshot img={store.overlay.screenshot} />
-          <Overlay />
-          {platform() !== 'darwin' && <WindowsButtons />}
-      </StyledApp>
+    <StyledApp>
+      <GlobalStyle />
+      <Toolbar />
+      <Line />
+      <Screenshot img={store.overlay.screenshot} />
+      {/* <Overlay /> */}
+      {platform() !== 'darwin' && <WindowsButtons />}
+    </StyledApp>
   );
 });
