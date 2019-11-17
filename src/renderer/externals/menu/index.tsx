436794282efde2fddb26e6a7d89c6b44fcc4008d
Ender <xenderpugx@gmail.com>
2019-11-17 16:13:55 +0000

    💬 Externals now have Roboto

--- src/renderer/externals/menu/index.tsx
+++ src/renderer/externals/menu/index.tsx
@@ -1,18 +1,35 @@
 import * as React from 'react';
-import { observer } from 'mobx-react';
-
-import { StyledApp } from './style';
-import { Menu } from './components/Menu';
 import * as ReactDOM from 'react-dom';
 
-export const App = observer(() => {
-    return (
-        <StyledApp>
-            <Menu />
-        </StyledApp>
-    )
-});
+import { App } from './components/App';
+import { fonts } from '../../constants';
+import { ipcRenderer } from 'electron';
 
-ReactDOM.render(<App />, document.getElementById("app"))
+ipcRenderer.setMaxListeners(0);
 
+const styleElement = document.createElement('style');
 
+styleElement.textContent = `
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: url(${fonts.robotoRegular}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  src: url(${fonts.robotoMedium}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 300;
+  src: url(${fonts.robotoLight}) format('woff2');
+}
+`;
+
+document.head.appendChild(styleElement);
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/renderer/externals/search/index.tsx
+++ src/renderer/externals/search/index.tsx
@@ -2,12 +2,38 @@ import * as React from 'react';
 
 import * as ReactDOM from 'react-dom';
 import Search from './app/index';
+import { fonts } from '../../constants';
+
+const styleElement = document.createElement('style');
+
+styleElement.textContent = `
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 400;
+  src: url(${fonts.robotoRegular}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 500;
+  src: url(${fonts.robotoMedium}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 300;
+  src: url(${fonts.robotoLight}) format('woff2');
+}
+`;
+
+document.head.appendChild(styleElement);
 
 const App = ({ style }: { style: any }) => {
   return <Search style={style} />;
 };
 
 ReactDOM.render(
-  <App style={{ overflow: 'hidden', margin: '0px', width: 'auto' }} />,
+  <App style={{ overflow: 'hidden', margin: '0px', width: 'auto', boxShadow: '0 0 0px 2px #b3cefb', fontFamily: 'Roboto' }} />,
   document.getElementById('app'),
 );
