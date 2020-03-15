26b67aa0dfe84b7b0f68bad74eaf38714eeb3b8d
EnderDev <xenderpugx@gmail.com>
2020-03-15 21:21:25 +0000

    🍱 Include Roboto Slab in NTP

--- src/renderer/views/newtab/index.tsx
+++ src/renderer/views/newtab/index.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
-import { render } from 'react-dom';
-import NewTab from './app/index';
+import * as ReactDOM from 'react-dom';
+import { App } from './app';
 import { fonts } from '../app/constants/fonts';
 import { createMount } from '~/shared/utils/webui';
 
@@ -27,6 +27,37 @@ styleElement.textContent = `
   src: url(${fonts.robotoLight}) format('woff2');
 }
 
+@font-face {
+  font-family: 'Roboto Slab';
+  font-style: normal;
+  font-weight: 600;
+  src: url(${fonts.slabBold}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto Slab';
+  font-style: normal;
+  font-weight: 500;
+  src: url(${fonts.slabMedium}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto Slab';
+  font-style: normal;
+  font-weight: 400;
+  src: url(${fonts.slabRegular}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto Slab';
+  font-style: normal;
+  font-weight: 300;
+  src: url(${fonts.slabLight}) format('woff2');
+}
+@font-face {
+  font-family: 'Roboto Slab';
+  font-style: normal;
+  font-weight: 200;
+  src: url(${fonts.slabThin}) format('woff2');
+}
+
 *::selection {
   background-color: #1a73e845;
 }
@@ -36,4 +67,4 @@ document.head.appendChild(styleElement);
 
 createMount(document);
 
-render(<NewTab />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
