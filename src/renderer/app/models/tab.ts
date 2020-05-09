d143442c05d896bddab95d420d57226156741cc0
EnderDev <xenderpugx@gmail.com>
2020-05-09 18:21:04 +0100

    🚚 Make Tab a model

--- src/renderer/app/models/tab.ts
+++ src/renderer/app/models/tab.ts
@@ -3,9 +3,16 @@ import { ViewCreateOptions } from "../../../interfaces/view";
 import { observable } from "mobx";
 
 export class Tab {
+    @observable
     public id: string;
+
+    @observable
     public url: string;
+
+    @observable
     public title: string;
+
+    @observable
     public status: 'loading' | 'idle' | 'crashed' | 'suspended';
 
     @observable
@@ -16,6 +23,15 @@ export class Tab {
         this.url = url;
 
         ipcRenderer.send('view-create', { id, url, active })
+
+        ipcRenderer.on(`view-data-updated-${this.id}`, (event, data) => {
+            for (const [key, value] of Object.entries(data)) {
+                console.log(key, value)
+                this[key] = data[key]
+            }
+
+            console.log(data, this)
+        })
     }
 
     public refresh() {
