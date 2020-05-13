a6fec761987b389ec68507569ed6dd00328412ff
EnderDev <xenderpugx@gmail.com>
2020-05-13 21:55:28 +0100

    💄 Make seperate sub-component for SearchIcon

---
+++ src/renderer/app/components/Addressbar/SearchIcon/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+import { StyledSearchIcon } from "./style";
+import { Icon } from "../../Icon";
+
+export const SearchIcon = () => (
+    <StyledSearchIcon>
+        <Icon icon={"search"} size={14} />
+    </StyledSearchIcon>
+)
---
+++ src/renderer/app/components/Addressbar/SearchIcon/style.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { StyledNavigationButton } from "../../NavigationButton/style";
+
+export const StyledSearchIcon = styled(StyledNavigationButton)`
+    height: 30px;
+    margin: 0 2px;
+    width: 34px;
+    position: absolute;
+    transition: 0.3s color;
+`;
