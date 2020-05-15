d66e700c38215536d22a2c98fa19bb16ac75b481
EnderDev <xenderpugx@gmail.com>
2020-05-15 21:22:37 +0100

    💄 Add security indicators to AddressBar

--- src/renderer/app/components/Addressbar/SearchIcon/index.tsx
+++ src/renderer/app/components/Addressbar/SearchIcon/index.tsx
@@ -3,8 +3,23 @@ import React from "react";
 import { StyledSearchIcon } from "./style";
 import { Icon } from "../../Icon";
 
-export const SearchIcon = () => (
-    <StyledSearchIcon>
-        <Icon icon={"search"} size={14} />
-    </StyledSearchIcon>
-)
+import dot from '../../../store';
+import { observer } from "mobx-react";
+import { NEWTAB_URL } from "../../../../constants/web";
+
+export const SearchIcon = observer(() => {
+    const url = dot.tabs.selectedTab && dot.tabs.selectedTab.url;
+    let icon = "search";
+
+    if(dot.tabs.selectedTab) {
+        if(url.startsWith("http")) icon = "alert-circle"
+        if(url.startsWith("https")) icon = "lock"
+        if(url == NEWTAB_URL) icon = "search"
+    }
+
+    return (
+        <StyledSearchIcon>
+            <Icon icon={icon} size={14} />
+        </StyledSearchIcon>
+    )
+})
