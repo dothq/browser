2e364f44dcbf560174039b0e1228fe5dac9f1f25
EnderDev <xenderpugx@gmail.com>
2020-05-16 21:19:42 +0100

    🚚 Move view events to seperate file

---
+++ src/main/tools/view.ts
@@ -0,0 +1,71 @@
+import { View } from "../view";
+import { appWindow } from "..";
+
+export const createView = (options) => {
+    const view = new View(options.id, options.url);
+
+    appWindow.views.push(view)
+
+    if(options.active == true) {
+        appWindow.window.setBrowserView(view.view)
+        appWindow.selectedId = view.id;
+    }
+
+    view.rearrange()
+}
+
+export const selectView = (id) => {
+    const view = appWindow.getViewFromId(id);
+    if(!view) return;
+
+    appWindow.selectedId = view.id;
+    appWindow.window.setBrowserView(view.view)
+
+    if(view.view.isDestroyed() == false) view.rearrange()
+}
+
+export const destroyView = (id) => {
+    const { view } = appWindow.getViewFromId(id)
+
+    view.destroy()
+}
+
+export const refreshView = (id, ignoreCache: boolean) => {
+    const { view } = appWindow.getViewFromId(id);
+    if(!view) return;
+
+    if(ignoreCache) return view.webContents.reloadIgnoringCache()
+    view.webContents.reload()
+}
+
+export const stopView = (id) => {
+    const { view } = appWindow.getViewFromId(id);
+    if(!view) return;
+
+    view.webContents.stop()
+}
+
+export const backView = (id) => {
+    const { view } = appWindow.getViewFromId(id);
+    if(!view) return;
+
+    view.webContents.canGoBack() && view.webContents.goBack()
+}
+
+export const forwardView = (id) => {
+    const { view } = appWindow.getViewFromId(id);
+    if(!view) return;
+
+    view.webContents.canGoForward() && view.webContents.goForward()
+}
+
+export const navigateView = (id, url) => {
+    const { view } = appWindow.getViewFromId(id);
+    if(!view) return;
+
+    view.webContents.loadURL(url)
+}
+
+export const closeApp = () => {
+    appWindow.window.close()
+}
