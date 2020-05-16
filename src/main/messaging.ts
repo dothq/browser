725030e927d812ed2df4a0232d59910a212723d4
EnderDev <xenderpugx@gmail.com>
2020-05-16 21:20:27 +0100

    💥 Update messaging file to introduce new event methods

--- src/main/messaging.ts
+++ src/main/messaging.ts
@@ -1,56 +1,28 @@
 import { ipcMain } from "electron";
-import { View } from "./view";
-import { appWindow } from ".";
-import { NAVIGATION_HEIGHT } from "../renderer/app/constants/window";
+
+import { 
+    createView, 
+    selectView, 
+    destroyView, 
+    refreshView, 
+    stopView, 
+    backView, 
+    forwardView, 
+    navigateView, 
+    closeApp 
+} from "./tools/view";
 
 export const startMessagingAgent = () => {
-    ipcMain.on('view-create', (e, options) => {
-        appWindow.lastViewId = appWindow.selectedId;
+    ipcMain.on('view-create', (e, options) => createView(options))
+    ipcMain.on('view-select', (e, id) => selectView(id))
+    ipcMain.on('view-destroy', (e, id) => destroyView(id))
+    
+    ipcMain.on('view-refresh', (e, id, ignoreCache?: boolean) => refreshView(id, ignoreCache))
+    ipcMain.on('view-stop', (e, id) => stopView(id))
+    ipcMain.on('view-back', (e, id) => backView(id))
+    ipcMain.on('view-forward', (e, id) => forwardView(id))
 
-        const view = new View(options.id, options.url);
+    ipcMain.on('view-navigate', (e, id, url) => navigateView(id, url))
 
-        appWindow.views.push(view)
-
-        if(options.active == true) {
-            appWindow.window.setBrowserView(view.view)
-            appWindow.selectedId = view.id;
-        }
-
-        view.rearrange()
-    })
-
-    ipcMain.on('view-select', (e, id) => {
-        const view = appWindow.getViewFromId(id);
-        if(!view) return;
-
-        appWindow.selectedId = view.id;
-        appWindow.window.setBrowserView(view.view)
-
-        if(view.view.isDestroyed() == false) view.rearrange()
-    })
-
-    ipcMain.on('view-destroy', (e, id) => {
-        appWindow.lastViewId = appWindow.selectedId;
-
-        const { view } = appWindow.getViewFromId(id)
-
-        view.destroy()
-    })
-
-    ipcMain.on('view-refresh', (e, id, ignoreCache?: boolean) => {
-        const { view } = appWindow.getViewFromId(id);
-        if(!view) return;
-
-        if(ignoreCache) return view.webContents.reloadIgnoringCache()
-        view.webContents.reload()
-    })
-
-    ipcMain.on('view-navigate', (e, id, url) => {
-        const { view } = appWindow.getViewFromId(id);
-        if(!view) return;
-
-        view.webContents.loadURL(url)
-    })
-
-    ipcMain.on('app-close', (e) => { appWindow.window.close() })
+    ipcMain.on('app-close', (e) => closeApp)
 }
