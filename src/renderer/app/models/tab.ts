08ba7d55ed16e6f91bd7b7e8b18d1b0300b5c0bc
EnderDev <xenderpugx@gmail.com>
2020-05-16 21:22:19 +0100

    ✨ Add properties for theme color, navigationStatus and favicon

--- src/renderer/app/models/tab.ts
+++ src/renderer/app/models/tab.ts
@@ -1,6 +1,8 @@
 import { ipcRenderer } from "electron";
 import { ViewCreateOptions } from "../../../interfaces/view";
 import { observable } from "mobx";
+import { BLUE_1 } from "../../constants/colors";
+import { NEWTAB_URL } from "../../constants/web";
 
 export class Tab {
     @observable
@@ -13,7 +15,16 @@ export class Tab {
     public title: string = "New Tab";
 
     @observable
-    public status: 'loading' | 'idle' | 'crashed' | 'suspended';
+    public status: 'loading' | 'idle' | 'crashed' | 'suspended' = 'loading';
+
+    @observable
+    public favicon: string;
+
+    @observable
+    public themeColor: string = BLUE_1;
+    
+    @observable
+    public navigationStatus: { canGoForward: boolean, canGoBack: boolean };
 
     @observable
     public inputFocused: boolean = true;
@@ -25,19 +36,35 @@ export class Tab {
         this.id = id;
         this.url = url;
 
+        console.log("tab created")
+
         ipcRenderer.send('view-create', { id, url, active })
 
         ipcRenderer.on(`view-data-updated-${this.id}`, (event, data) => {
             for (const [key, value] of Object.entries(data)) {
-                console.log(key, value)
+                console.log(`tab.${key}`, "=>", data[key])
                 this[key] = data[key]
             }
-
-            console.log(data, this)
         })
+    }
+
+    public get isNTP() {
+        return this.url == NEWTAB_URL
     }
 
     public refresh() {
         ipcRenderer.send('view-refresh', this.id);
+    }
+
+    public stop() {
+        ipcRenderer.send('view-stop', this.id);
+    }
+
+    public goBack() {
+        ipcRenderer.send('view-back', this.id);
+    }
+
+    public goForward() {
+        ipcRenderer.send('view-forward', this.id);
     }
 }
