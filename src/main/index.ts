15b5164f3af275b86fbd61c241864b0b53d16439
EnderDev <xenderpugx@gmail.com>
2020-05-18 23:55:42 +0100

    ⬇️ Add autoUpdater to Dot

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,10 +1,13 @@
 import { AppWindow } from "./app";
 import { app, ipcMain } from "electron";
+import { autoUpdater } from 'electron-updater';
 
 export let appWindow: AppWindow;
 
 ipcMain.setMaxListeners(0);
 app.allowRendererProcessReuse = true;
+
+autoUpdater.checkForUpdatesAndNotify();
 
 if(process.env.ENV == "development") process.env.ELECTRON_DISABLE_SECURITY_WARNINGS = true.toString();
 
