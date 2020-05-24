e4779ac46e74c052391c0bcb32e476476081fdc8
EnderDev <xenderpugx@gmail.com>
2020-05-24 16:02:15 +0100

    💄 Add isDotPage and isFocused to Addressbar and Parts

--- src/renderer/app/components/Addressbar/Parts/index.tsx
+++ src/renderer/app/components/Addressbar/Parts/index.tsx
@@ -4,13 +4,14 @@ import { StyledParts, Part } from "./style";
 import dot from '../../../store';
 import { observer } from "mobx-react";
 
-export const Parts = observer(() => {
+export const Parts = observer(({ isDotPage }: { isDotPage: boolean }) => {
     const url = dot.tabs.selectedTab && dot.tabs.selectedTab.url;
     const isFocused = dot.tabs.selectedTab && !dot.tabs.selectedTab.inputFocused
 
     return (
         <StyledParts
             visible={isFocused}
+            isDotPage={isDotPage}
         >
             {dot.addressbar.urlParts(url).map(part => ( 
                 <Part key={part.id} opacity={part.opacity}>{part.value}</Part> 
--- src/renderer/app/components/Addressbar/index.tsx
+++ src/renderer/app/components/Addressbar/index.tsx
@@ -6,9 +6,12 @@ import dot from '../../store'
 import { Parts } from './Parts';
 import { SearchIcon } from './SearchIcon';
 import { FavouriteIcon } from './FavouriteIcon';
+import { EXPO_URL } from '../../../constants/web';
 
 export const Addressbar = observer(() => {
     const events = dot.events;
+    const url = dot.tabs.selectedTab && dot.tabs.selectedTab.url;
+    const isFocused = dot.tabs.selectedTab && dot.tabs.selectedTab.inputFocused
 
     return (
         <StyledAddressbar>
@@ -22,8 +25,10 @@ export const Addressbar = observer(() => {
                 onKeyUp={(event) => events.inputOnKeyUp(event)}
                 onChange={(event) => events.inputOnChange(event)}
                 value={dot.addressbar.inputValue()}
+                isDotPage={url && url.startsWith(EXPO_URL)}
+                isFocused={isFocused}
             />
-            <Parts />
+            <Parts isDotPage={url && url.startsWith(EXPO_URL)} />
             <FavouriteIcon />
         </StyledAddressbar>
     )
