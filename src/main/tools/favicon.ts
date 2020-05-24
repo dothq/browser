67000e53a7881b5b07eed09ecca48a0a5be437ea
EnderDev <xenderpugx@gmail.com>
2020-05-24 16:48:41 +0100

    💥 Allow Data URLs to be favicons

--- src/main/tools/favicon.ts
+++ src/main/tools/favicon.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
+const dataURLRegex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?,[a-z0-9\!\$\&\'\,\(\)\*\+\,\;\=\-\.\_\~\:\@\/\?\%\s]*\s*$/i
+
 export const downloadFaviconFromUrl = async (url) => {
     try {
+        if(url.match(dataURLRegex)[1].startsWith("image/")) return url;
+
         const res = await axios.get(url, { responseType: 'arraybuffer' })
 
         let buffer = Buffer.from(res.data, 'binary');
