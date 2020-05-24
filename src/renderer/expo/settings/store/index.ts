07882207c92872ce10c62a387484675230f433dc
EnderDev <xenderpugx@gmail.com>
2020-05-24 16:04:17 +0100

    💬 Make the "home" section's name "General"

--- src/renderer/expo/settings/store/index.ts
+++ src/renderer/expo/settings/store/index.ts
@@ -3,25 +3,23 @@ import React from "react";
 import { observable } from 'mobx';
 
 class Dot {
-    private originalURL = window.location.href;
-
     @observable
     public selectedSection: 'home' | 'appearance' | 'search' | 'downloads' | 'languages' | 'user' | 'about' = 'home'
 
     public get sectionName() {
-        if(this.selectedSection == "home") return "Settings"
+        if(this.selectedSection == "home") return "General"
         if(this.selectedSection == "appearance") return "Appearance"
         if(this.selectedSection == "search") return "Search Engines"
         if(this.selectedSection == "downloads") return "Downloads"
         if(this.selectedSection == "languages") return "Region and Languages"
-        if(this.selectedSection == "user") return "Me"
+        if(this.selectedSection == "user") return "Profile"
         if(this.selectedSection == "about") return "About Dot Browser"
         return "Unknown"
     }
 
     constructor() {
         window.addEventListener('DOMContentLoaded', () => {
-            document.title = this.sectionName
+            document.title = "Settings"
         })
     }
 }
