7e2b6e4193bdb15113e51edab9c049db4d6bdf6e
EnderDev <xenderpugx@gmail.com>
2020-05-24 17:17:55 +0100

    🐛 Fix favicon not loading on other pages

--- src/main/tools/favicon.ts
+++ src/main/tools/favicon.ts
@@ -4,16 +4,19 @@ const dataURLRegex = /^\s*data:([a-z]+\/[a-z]+(;[a-z\-]+\=[a-z\-]+)?)?(;base64)?
 
 export const downloadFaviconFromUrl = async (url) => {
     try {
-        if(url.match(dataURLRegex)[1].startsWith("image/")) return url;
+        if(url.match(dataURLRegex) && url.match(dataURLRegex)[1].startsWith("image/")) {
+            return url;
+        } else {
+            const res = await axios.get(url, { responseType: 'arraybuffer' })
 
-        const res = await axios.get(url, { responseType: 'arraybuffer' })
-
-        let buffer = Buffer.from(res.data, 'binary');
-    
-        const favicon = `data:${res.headers['content-type']};base64,${buffer.toString('base64')}`
+            let buffer = Buffer.from(res.data, 'binary');
         
-        return favicon;
+            const favicon = `data:${res.headers['content-type']};base64,${buffer.toString('base64')}`
+            
+            return favicon;
+        }
     } catch(error) {
+        console.log(error)
         return null;
     }
 }
