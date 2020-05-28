f1f822ac9fa500f14ac5caf34ed2996786b5fa4a
EnderDev <xenderpugx@gmail.com>
2020-05-28 17:04:16 +0100

    ✨ Inject DotURL to dot:// pages for getting URL parameters

--- src/main/tools/protocol.ts
+++ src/main/tools/protocol.ts
@@ -10,7 +10,9 @@ export const startProtocolService = () => {
         ses.protocol.registerBufferProtocol('dot', (request, callback) => {
             const parsed = parse(request.url)
 
-            callback({ mimeType: 'text/html', data: Buffer.from(`<script>document.title = "${parsed.host.substr(0, 1).toUpperCase() + parsed.host.substr(1, parsed.host.length)}"</script><iframe src="http://localhost:9015/${parsed.path == "/" ? parsed.host + ".html" : parsed.path.split("/")[1] + `?t=${Date.now()}`}" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">`) })
+            console.log(parsed)
+
+            callback({ mimeType: 'text/html', data: Buffer.from(`<script>document.title = "${parsed.host.substr(0, 1).toUpperCase() + parsed.host.substr(1, parsed.host.length)}"; window.dotURL = JSON.parse(\`${JSON.stringify(parsed)}\`)</script><iframe src="http://localhost:9015/${parsed.path == "/" ? parsed.host + ".html" : parsed.path.split("/")[1] + `?t=${Date.now()}`}" style="position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;">`) })
         }, (error) => {
           if (error) console.error('Failed to register protocol')
         })
