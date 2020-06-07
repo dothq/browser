f2e78b1bda070abb57d479ab73d441bca567e87f
EnderDev <xenderpugx@gmail.com>
2020-06-07 17:43:03 +0100

    💄 Add LargeCard and SmallCard component + styles

---
+++ src/renderer/expo/newtab/components/Cards/Large/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const StyledLargeCard = styled.div`
+    width: 500px;
+    height: 328px;
+
+    background: linear-gradient(180.32deg, #ffffff00 0%, rgb(0, 0, 0) 100%);
+    box-shadow: 0px 1px 22px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+
+    margin: 26px;
+`;
