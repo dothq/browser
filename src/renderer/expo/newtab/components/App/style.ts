c65799433a5cc22838e4b9f031794092e3b56d68
EnderDev <xenderpugx@gmail.com>
2020-06-07 17:41:56 +0100

    💄 Add App component and styles

---
+++ src/renderer/expo/newtab/components/App/style.ts
@@ -0,0 +1,32 @@
+import styled, { css } from "styled-components";
+
+export const Style = css`
+    body {
+        margin: 0;
+        padding: 0;
+        font-family: system-ui;
+        background-color: white;
+    }
+
+    *::selection {
+        background-color: rgba(190, 215, 248, 0.4)	
+    }
+    
+    * {
+        -webkit-user-drag: none;
+        -khtml-user-drag: none;
+        -moz-user-drag: none;
+        -o-user-drag: none;
+        user-drag: none;
+    }
+
+    *:not(input) {
+        user-select: none;
+    }
+`;
+
+export const StyledApp = styled.div`
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
