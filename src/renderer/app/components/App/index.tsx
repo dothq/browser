cc24c3e8ac85516a1d1a48cb1f511c9e4099d702
EnderDev <xenderpugx@gmail.com>
2020-06-28 20:00:39 +0100

    🔥 Remove Inter font from App

--- src/renderer/app/components/App/index.tsx
+++ src/renderer/app/components/App/index.tsx
@@ -4,92 +4,14 @@ import { createGlobalStyle } from 'styled-components';
 
 import { Tabs } from '../Tabs';
 import { Navigation } from '../Navigation';
+import { Bookmarks } from '../Bookmarks';
 
 import dot from '../../store'
 import { observer } from 'mobx-react';
 
-import { fonts } from "../../../constants/fonts";
 import { remote } from 'electron';
 
 const GlobalStyle = createGlobalStyle`${Style}`;
-
-const style = document.createElement('style')
-
-style.textContent = `
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 100;
-    font-display: swap;
-    src: url(${fonts.inter.thin.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 200;
-    font-display: swap;
-    src: url(${fonts.inter.extralight.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 300;
-    font-display: swap;
-    src: url(${fonts.inter.light.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 400;
-    font-display: swap;
-    src: url(${fonts.inter.regular.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 500;
-    font-display: swap;
-    src: url(${fonts.inter.medium.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 600;
-    font-display: swap;
-    src: url(${fonts.inter.semibold.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 700;
-    font-display: swap;
-    src: url(${fonts.inter.bold.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 800;
-    font-display: swap;
-    src: url(${fonts.inter.extrabold.font}) format("woff2");
-}
-
-@font-face {
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 900;
-    font-display: swap;
-    src: url(${fonts.inter.black.font}) format("woff2");
-}
-`
-
-document.head.appendChild(style)
 
 Array.from(Array(9).keys()).forEach(n => {
     remote.globalShortcut.register(`CmdOrCtrl+${n+1}`, () => {
@@ -108,6 +30,7 @@ export const App = observer(() => (
         <GlobalStyle />
         <Tabs />
         <Navigation />
+        {dot.dbReady && dot.db.settings.appearance.showBookmarksBar && <Bookmarks />}
         <Line fromTop={dot.navigationHeight} />
     </StyledApp>
 ))
