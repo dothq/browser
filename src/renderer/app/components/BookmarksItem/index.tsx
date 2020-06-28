060b3ef847f25740879189289d286b554e22b92e
EnderDev <xenderpugx@gmail.com>
2020-06-28 20:02:13 +0100

    💄 Add Bookmarks and BookmarksItem component

---
+++ src/renderer/app/components/BookmarksItem/index.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+import { observer } from "mobx-react";
+import { ipcRenderer } from "electron";
+
+import { StyledBookmarksItem, BookmarksIcon, BookmarksTitle } from "./style";
+
+import dot from '../../store'
+
+export const BookmarksItem = observer(({ url, title, favicon }: { url: string; title?: string; favicon: string }) => {
+    const tab = dot.tabs.selectedTab
+
+    const onBookmarksItemMouseUp = (e, url) => {
+        if(e.button == 0) return tab.goto(url) // Primary click
+        if(e.button == 1) return dot.tabs.add({ url, active: false }) // Middle click
+        if(e.button == 2) return ipcRenderer.send('bookmark-menu-popup', { bookmark: { url, title, favicon }, x: e.clientX, y: e.clientY })
+    }
+
+    return (
+        <StyledBookmarksItem 
+            onMouseUp={(e) => onBookmarksItemMouseUp(e, url)}
+            title={`${title || ''}${title ? '\n' : ''}${url || ''}`}
+        >
+            <BookmarksIcon icon={favicon} />
+            {title && title.length > 0 && <BookmarksTitle>{title}</BookmarksTitle>}
+        </StyledBookmarksItem>
+    )
+})
---
+++ src/renderer/app/components/BookmarksItem/style.ts
@@ -0,0 +1,36 @@
+import styled, { css } from "styled-components";
+
+export const StyledBookmarksItem = styled.div`
+    height: 26px;
+    padding: 0 8px;
+    border-radius: 26px;
+    display: flex;
+    margin: 0 2px;
+    align-items: center;
+    transition: 0.1s background-color;
+
+    &:hover {
+        background-color: rgba(0, 0, 0, 0.05);
+    }
+
+    &:active {
+        background-color: rgba(0, 0, 0, 0.10);
+    }
+`;
+
+export const BookmarksIcon = styled.div`
+    width: 16px;
+    height: 16px;
+
+    ${({ icon }: { icon: any }) => css`
+        background-image: url(${icon});
+        background-size: cover;
+        background-repeat: no-repeat;
+    `}
+`;
+
+export const BookmarksTitle = styled.div`
+    font-size: 12px;
+    color: #000;
+    margin-left: 8px;
+`;
