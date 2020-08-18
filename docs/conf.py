6448e446d206fecd8191a4d69d9d39b7ce70ee1d
Kai Lyons <62919224+KaiLikesLinux@users.noreply.github.com>
2020-08-18 14:38:42 -0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,9 @@
 import sphinx_documatt_theme
 extensions = ["sphinx_documatt_theme"]
 html_theme = "sphinx_documatt_theme"
+
+templates_path = ['_templates']
+project = 'DotBrowser'
+copyright = '2020, DotHQ'
+author = 'DotHQ and Kai Lyons'
+show_authors = False
