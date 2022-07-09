f8432600da5daf3f02c9d36c5d4b8cb864fbb372
EnderDev <42723993+EnderDev@users.noreply.github.com>
2022-07-09 23:33:33 +0100

    🏁 Stop if Mozillabuild is not installed on Windows

--- scripts/pull-ff.js
+++ scripts/pull-ff.js
@@ -2,13 +2,39 @@ import { execa } from "execa";
 import { existsSync, writeFileSync } from "fs";
 import { resolve } from "path";
 import axios from "axios";
-import { tmpdir } from "os";
+import { platform, tmpdir } from "os";
 import { exit } from "process";
+
+const sh = (bin, args) => {
+    if (platform() == "win32") {
+        args = ["-c", `${[bin, ...args].join(" ")}`];
+        bin = resolve("C:", "mozilla-build", "start-shell.bat");
+
+        console.warn(`Running command inside Mozillabuild...`);
+    }
+
+    return execa(bin, args);
+}
 
 const main = async () => {
     if (existsSync(resolve(process.cwd(), "core"))) {
 
     } else {
+        if (platform() == "win32") {
+            if (!existsSync(resolve("C:", "mozilla-build", "start-shell.bat"))) {
+                console.error(`Mozillabuild is not installed!`);
+                console.error(``);
+                console.error(`In order to build on a Windows machine, it is required`)
+                console.error(`that any commands relating to the engine should be executed`)
+                console.error(`within the Mozillabuild shell to reduce incompabilities.`)
+                console.error(``);
+                console.error(`Please install it from https://ftp.mozilla.org/pub/mozilla/libraries/win32/MozillaBuildSetup-Latest.exe,`);
+                console.error(`and re-run this command from the Mozillabuild shell at ${resolve("C:", "mozilla-build", "start-shell.bat")}`)
+            
+                exit(1);
+            }
+        }
+
         console.warn(`Since Firefox isn't in the tree yet, we will need to clone it. This may take a few minutes to a few hours.`)
 
         const res = await axios.get(
@@ -30,7 +56,8 @@ const main = async () => {
 
         writeFileSync(resolve(tmpdir(), "bootstrap.py"), res.data);
 
-        await execa("python3", [bootstrapPath, "--vcs=git", "--no-interactive"], { stdio: "inherit" });
+        await sh("git", ["config", "fetch.prune", "true"]);
+        await sh("python3", [bootstrapPath, "--vcs=git", "--no-interactive"], { stdio: "inherit" });
     }
 }
 
